8b1e1641148c7329b0f3b0d111dcf3149f28c2cd
Navruzjon Uktamjonov <104247975+Nikejan@users.noreply.github.com>
2022-12-14 05:32:05 -0600

    Update settings.py

--- NewYear/settings.py
+++ NewYear/settings.py
@@ -129,9 +129,9 @@ STATIC_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 
 
