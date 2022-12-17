b2a68fc3f0a201607fb963e46d6b3e37eea80803
Navruzjon Uktamjonov <104247975+Nikejan@users.noreply.github.com>
2022-12-17 12:31:00 +0500

    Update settings.py

--- NewYear/settings.py
+++ NewYear/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'NewYear.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ATjqF84KH1lGFIIsfOnp',
+        'HOST': 'containers-us-west-92.railway.app',
+        'PORT': '5471',
     }
 }
 
