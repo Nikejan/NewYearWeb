ba583f889ad90ee11a61556ee00d43e7347793a2
Navruzjon Uktamjonov <104247975+Nikejan@users.noreply.github.com>
2022-12-18 16:04:54 -0600

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "image")
+    list_display = ("name", "image_url")
 admin.site.register(Product, ProductAdmin)
 
 
