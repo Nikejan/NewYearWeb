a3d8d2af1a53e62bd07d9c9da2d21e18d1f8f6d8
Navruzjon Uktamjonov <104247975+Nikejan@users.noreply.github.com>
2023-01-02 23:16:39 -0600

    Update models.py

--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=7, decimal_places=0)
     digital = models.BooleanField(default=False, null=True, blank=True)
-    image = models.CharField(max_length=1087)
+    image = models.ImageField()
     
     class Meta:
           verbose_name_plural = "store"
