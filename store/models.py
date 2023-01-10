8cf29f99c216d0f9eee56aea7e647bd607480527
Navruzjon Uktamjonov <104247975+Nikejan@users.noreply.github.com>
2023-01-10 23:54:05 +0500

    Update models.py

--- store/models.py
+++ store/models.py
@@ -4,9 +4,6 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=10087)
-    price = models.DecimalField(max_digits=7, decimal_places=0)
-    image = models.ImageField()
-    gentra = models.CharField(max_length=200)
     
     class Meta:
           verbose_name_plural = "store"
